242931b6529a1161f0936b08196b389fd2eda2ff
Alejandro Herrera Merlo <alex13pks@gmail.com>
2022-12-27 17:31:58 -0600

    Added console error

--- src/main.js
+++ src/main.js
@@ -20,14 +20,51 @@ Split({
   ],
 });
 
-const codeEditor = editor.create(document.getElementById('editor'), {
-  value: '',
+const outputDiv = document.querySelector('#output');
+const editorDiv = document.querySelector('#editor');
+const codeEditor = editor.create(editorDiv, {
+  value: `const HelloWorld = () => {
+    console.log('Hello world')
+  }
+HelloWorld()`,
   theme: 'vs-dark',
+  fontFamily: "'Fira Code', 'Consolas'",
+  fontLigatures: true,
   language: 'javascript',
   automaticLayout: true,
   wordWrap: 'on',
 });
 
+// Override the console.log function to display the output in the output div
+const oldLog = console.log;
+const oldError = console.error;
+console.log = function (...args) {
+  oldLog(...args);
+  outputDiv.innerHTML = `<p>${args.join(' ')}</p>`;
+};
+
+console.error = function (...args) {
+  oldError(...args);
+  outputDiv.innerHTML = `<p class="text-red-500">${args.join(' ')}</p>`;
+};
+
+function executeCode() {
+  try {
+    const code = codeEditor.getValue();
+    eval(code);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+let timeout;
 codeEditor.onDidChangeModelContent(() => {
-  console.log(codeEditor.getValue());
+  clearTimeout(timeout);
+  timeout = setTimeout(() => {
+    executeCode();
+  }, 300);
 });
+
+function clearOutput() {}
+
+executeCode();
