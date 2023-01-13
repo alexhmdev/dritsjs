dc18adc2bdc09ab9d2cefcc0d5f9f7c87e4c99e0
AlexHM <alex13pks@gmail.com>
2023-01-13 00:19:12 -0600

    Removed out dir

--- out/preload/preload.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const { contextBridge, ipcRenderer } = require("electron");
-contextBridge.exposeInMainWorld("electronAPI", {
-  minimize: () => ipcRenderer.send("minimize"),
-  closeWindow: () => ipcRenderer.send("closeWindow"),
-  maxUnmaxWindow: () => ipcRenderer.send("maxUnmaxWindow"),
-  isMaximized: () => ipcRenderer.invoke("isMaximized"),
-  openMenu: (x, y) => ipcRenderer.invoke("openMenu", x, y)
-});
